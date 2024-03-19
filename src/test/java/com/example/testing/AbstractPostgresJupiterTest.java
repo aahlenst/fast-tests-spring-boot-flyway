5bc04b8be588a7615f400fd370f9e9d6bcaae488
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2024-03-19 14:28:32 +0100

    Update PostgreSQL to v16.2

--- src/test/java/com/example/testing/AbstractPostgresJupiterTest.java
+++ src/test/java/com/example/testing/AbstractPostgresJupiterTest.java
@@ -14,7 +14,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
  * documentation</a> for details about singleton containers.
  */
 public class AbstractPostgresJupiterTest implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-	static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:16.1");
+	static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:16.2");
 
 	static {
 		postgres.start();
