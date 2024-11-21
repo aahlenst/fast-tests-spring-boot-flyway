f4f7202d4ae8fc6706d33bff832aa4159c69ae57
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2024-11-21 13:45:54 +0100

    Update PostgreSQL to v17.1

--- src/test/java/com/example/testing/AbstractPostgresJupiterTest.java
+++ src/test/java/com/example/testing/AbstractPostgresJupiterTest.java
@@ -14,7 +14,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
  * documentation</a> for details about singleton containers.
  */
 public class AbstractPostgresJupiterTest implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-	static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:17.0");
+	static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:17.1");
 
 	static {
 		postgres.start();
